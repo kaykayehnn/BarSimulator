14a70603780a7bc802b3786bbcd185c061b3ff1a
Krasimir Nedelchev <kaykayehnn@gmail.com>
2021-11-10 18:01:33 +0200

    Add assignment description

--- BarSimulator/Program.cs
+++ BarSimulator/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Threading;
 
@@ -6,6 +6,18 @@ namespace BarSimulator
 {
     class Program
     {
+        /*
+         * Take the Bar simulator example from the lectures and extend it with:
+            1. Drinks: there are some drinks on sale, each with price and quantity in stock.
+               Visitors can choose a drink to get and purchase it.
+            2. Visitors have a budget to spent.
+            3. Visitors have age and the Bar doesn't let in people below 18.
+            4. Visitors that wait in the queue may decide (randomly) to leave the queue and walk
+               the streets again. Bonus points if the longer they wait, the more they are likely
+               to leave the queue.
+            5. Bar can close at some point and kick everyone out, then it won't let anyone in.
+            6. Bar reports all sales and which drinks are out of stock
+        */
         static void Main(string[] args)
         {
             // We simulate night life of students.
